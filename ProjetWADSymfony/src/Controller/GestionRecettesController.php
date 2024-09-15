c6cfeb391df532dad1831d1718601c66e6c25059
charifaZnifech <charifaznifech01@gmail.com>
2024-09-15 20:29:15 +0200

    afficher tt les recettes

--- ProjetWADSymfony/src/Controller/GestionRecettesController.php
+++ ProjetWADSymfony/src/Controller/GestionRecettesController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class GestionRecettesController extends AbstractController
 {
-    #[Route('/gestion/recettes', name: 'app_gestion_recettes')]
-    public function index(): Response
-    {
-        return $this->render('gestion_recettes/index.html.twig', [
-            'controller_name' => 'GestionRecettesController',
-        ]);
-    }
+    #[Route('/gestion/recettes/afficher')]
+      
+   public function selectAll(RecetteRepository $recettes)
+   {
+       $ALLRecettes = $recettes->findAll();
+
+       return $this->render('gestion_recettes/afficherRcettes.html.twig', [
+           'recettes' => $ALLRecettes
+       ]);
+   }
+
 }
