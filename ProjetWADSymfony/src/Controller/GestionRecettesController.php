a4c1153c5cf4172139a86f338f80b7b070e77731
Charifa Znifech <c.znifech@interface3.be>
2024-09-19 16:27:31 +0200

    fin push 19/09

--- ProjetWADSymfony/src/Controller/GestionRecettesController.php
+++ ProjetWADSymfony/src/Controller/GestionRecettesController.php
@@ -2,21 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Recette;
+use App\Form\AjouterRecetteFormType;
 use App\Repository\RecetteRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class GestionRecettesController extends AbstractController
 {
-    #[Route('/gestion/recettes/afficher')]
-      
+    public ManagerRegistry $doctrine;
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+    #[Route('/gestion/recettes/afficher')] 
    public function selectAll(RecetteRepository $recettes)
    {
        $ALLRecettes = $recettes->findAll();
 
        return $this->render('gestion_recettes/afficherRcettes.html.twig', [
            'recettes' => $ALLRecettes]);
+   }
+
+   #[Route('/gestion/recettes/ajouter', name : 'ajouter')]
+   public function ajouter(Request $req)
+   {
+      $recette = new Recette();
+      $form = $this->createForm(AjouterRecetteFormType::class, $recette);
+      $form->handleRequest($req);
+
+      if($form->isSubmitted() && $form->isValid()){
+          $em = $this->doctrine->getManager();
+          $em->persist($recette);
+          $em->flush();
+      }
+      $vars = ['form']
+
+
+      
+       
+
+
+       return $this->render('gestion_recettes/ajouterRecette.html.twig');
    }
 
 }
